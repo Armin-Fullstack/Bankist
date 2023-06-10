b843c8ef6a3d8b3ee810acf6ed3b0c947da9cbc5
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-06-10 12:42:35 +0330

    feat: created status details

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const nightBtn = document.querySelector(".night-btn")
 const dayBtn = document.querySelector(".day-btn")
 const statusBtn = document.querySelectorAll(".status-btn")
+const statusBtnBorder = document.querySelector(".status-btn-border")
 
 const nightDayContainer = document.querySelector(".night-day-container")
 
@@ -13,15 +14,13 @@ nightBtn.addEventListener("click" , () => {
  nightDayContainer.classList.toggle("active-mode")
 })
 
-
 // Status Button functionality
-statusBtn.forEach(button => {
-  button.addEventListener("click" , (e) => {
-    if(!e.target.classList.contains("active-status-button")) {
-      statusBtn.forEach(element => {
-        element.classList.remove("active-status-button")
-      })
-      button.classList.add("active-status-button")
-    }
+statusBtn.forEach((button , i) => {
+  button.addEventListener("click" , () => {
+    statusBtn.forEach(element => {
+      element.classList.remove("active-btn")
+    })
+    button.classList.add("active-btn")
+    statusBtnBorder.style.left = `calc(calc(100% / 4) * ${i})`
   })
 })
